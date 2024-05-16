05bbb2e0a08f00718701376e7461c5ed58742349
tam11a <ibrahimsadiktamim@gmail.com>
2024-05-17 03:32:07 +0600

    updated: MQTT BROKER URL

--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -6,13 +6,16 @@ export class MqttService implements OnModuleInit {
   mqttClient: MqttClient;
 
   onModuleInit() {
-    this.mqttClient = connect('mqtt://127.0.0.1:1883', {
-      clean: true,
-      connectTimeout: 4000,
-      // username: this.configService.get<string>('username'),
-      // password: this.configService.get<string>('password'),
-      reconnectPeriod: 1000,
-    });
+    this.mqttClient = connect(
+      process.env.MQTT_BROKER ?? 'mqtt://127.0.0.1:1883',
+      {
+        clean: true,
+        connectTimeout: 4000,
+        // username: this.configService.get<string>('username'),
+        // password: this.configService.get<string>('password'),
+        reconnectPeriod: 1000,
+      },
+    );
 
     this.mqttClient.on('connect', function (hello: Packet) {
       console.info('Connected to CloudMQTT', hello);
