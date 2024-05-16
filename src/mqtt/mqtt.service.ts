9f1a6eb1e2d805767f49bc71037d58517c7e4ca4
tam11a <ibrahimsadiktamim@gmail.com>
2024-05-17 03:27:51 +0600

    updated: mqtt url

--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -6,12 +6,12 @@ export class MqttService implements OnModuleInit {
   mqttClient: MqttClient;
 
   onModuleInit() {
-    this.mqttClient = connect('mqtt://localhost:1883', {
+    this.mqttClient = connect('mqtt://127.0.0.1:1883', {
       clean: true,
       connectTimeout: 4000,
       // username: this.configService.get<string>('username'),
       // password: this.configService.get<string>('password'),
-      // reconnectPeriod: 1000,
+      reconnectPeriod: 1000,
     });
 
     this.mqttClient.on('connect', function (hello: Packet) {
